fc772f59c6b85cc93514a221a998c733ef03e8ce
Aditya <aditya.kumar@nativebyte.in>
2024-04-06 21:45:52 +0530

    landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,15 +7,19 @@ import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
   {
-    title: "About",
+    title: "WHO WE ARE",
     path: "#about",
   },
   {
-    title: "Projects",
+    title: "FOR ORGANIZATION",
     path: "#projects",
   },
   {
-    title: "Contact",
+    title: "IMPACT REPORT",
+    path: "#contact",
+  },
+  {
+    title: "IMPACT REPORT",
     path: "#contact",
   },
 ];
@@ -24,7 +28,7 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState<any>(false);
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#FFAF00] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
